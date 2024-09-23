26d548f6c4d5b8250f6d84830fa722a9ca73d165
Ali Aliyev <eli.eliyev.200424@gmail.com>
2024-09-23 10:49:42 +0400

    Update AppDbContext.cs

--- ConsoleApp4/AppDbContext.cs
+++ ConsoleApp4/AppDbContext.cs
@@ -11,7 +11,7 @@ public class AppDbContext:DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(@"Your_Connection_String");
+        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
